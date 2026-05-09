3165c95913029b472f28748b8108ca60aec60a90
siren8289 <siren8289@gmail.com>
2026-05-09 23:53:22 +0900

    자바 구축

---
+++ back/src/test/java/com/pivotseoul/PivotSeoulApplicationTests.java
@@ -0,0 +1,12 @@
+package com.pivotseoul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PivotSeoulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
